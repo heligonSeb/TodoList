4f4de9d6e2177d0ea825fab4780f67e7360cc910
heligonSeb <heligon.seb@gmail.com>
2023-07-26 15:59:14 +0200

    begin test on default controller

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function homepageRenderTest(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !");
+    }
+}
+
